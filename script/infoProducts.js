482d02edb9993dfe179783ed91201c27570e9c5c
ingrid4424 <stepha4424@gmail.com>
2022-10-31 22:37:25 -0500

    detalles descripción productos

--- script/infoProducts.js
+++ script/infoProducts.js
@@ -1,46 +1,67 @@
 const productsInfo = [{
     name: 'Camiseta Mirá ve',
     img: "../../src/img/store/Camiseta1.jpg",
-    details: 'Aqui va descripión y detalle productos',
+    details: 'Camiseta de negra 100% algodón con utilizando la tecnica del estampado  en serigrafía  para mayor duración',
     price: '40.000',
     review: '5'
+    ,withDiscount:true,
+    discountAverage:'5%',
 },
 {
     name: 'Camiseta tortuga',
     img: "../../src/img/store/Camiseta2.jpg",
-    details: 'Aqui va descripión y detalle productos',
+    details: 'Camiseta de negra 100% algodón con utilizando la tecnica del estampado en serigrafía para mayor duración ideal para tardear y llevar la escencia de la sucursal',
     price: '35.000',
     review: '4'
+    ,withDiscount:false,
+    discountAverage:'0%',
 
    
 },
 {
     name: 'Camiseta Papaya',
     img: "../../src/img/store/Camiseta4.jpg",
-    details: 'Lorem Impsum Aqui va descripión y detalle productos',
+    details: 'Camiseta de negra 100% algodón con utilizando la tecnica del estampado  en serigrafía  para mayor duración, representando un dicho famoso Colombiano',
     price: '10.000',
     review: '5'
-
+    ,withDiscount:true,
+    discountAverage:'40%',
 },
 {
     name: 'Camiseta Ya te Caigo',
     img: "../../src/img/store/Camiseta5.jpg",
-    details: 'Lorem Impsum Aqui va descripión y detalle productos',
+    details: 'Camiseta de negra 100% algodón con utilizando la tecnica del estampado  en serigrafía  para mayor duración, representando un dicho famoso Colombiano',
     price: '10.000',
     review: '5'
+    ,withDiscount:true,
+    discountAverage:'40%',
 },
 {
     name: 'Camiseta marimba',
     img: "../../src/img/store/Camiseta6.jpg",
-    details: 'Lorem Aqui va descripión y detalle productos',
+    details: 'Camiseta negra 100% algodón representando elementos del pacífico Colombiano. Esta camiseta utiliza la tecnica del estampado en serigrafía para mayor calidad y duración',
     price: '35.000',
     review: '5'
+    ,withDiscount:false,
+    discountAverage:'0%',
 },
 {
     name: 'Camiseta chiva',
     img: "../../src/img/store/Camiseta6.jpg",
-    details: 'Lorem Impsum Aqui va descripión y detalle productos',
+    details: 'Camiseta negra 100% algodón representando elementos autoctonos de Colombia. Esta camiseta utiliza la tecnica del estampado en serigrafía para mayor calidad y duración',
     price: '40.000',
     review: '4'
+    ,withDiscount:false,
+    discountAverage: '0%',
+},{
+    name: 'Gorra',
+    img: "https://img.ltwebstatic.com/images3_pi/2022/09/27/166426687490e68746eb97e1a6201146a8e7050526_thumbnail_600x.webp",
+    details: 'Gorra color caqui perfecta para llevar de aventura',
+    price: '40.000',
+    review: '5'
+    ,withDiscount:true,
+    discountAverage:'5%',
 },
+
+
 ]
